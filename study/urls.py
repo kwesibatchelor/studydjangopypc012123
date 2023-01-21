4c29784fcd4d008251fbadfe8887e672818a329f
kwesibatchelor <kwesibatchelor@gmail.com>
2023-01-21 14:24:27 -0500

    first commit five approx attempts

---
+++ study/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Kwesi')
+
+def room(request):
+    return HttpResponse('ROOM')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home), 
+    path('room/', room), 
+
+]
